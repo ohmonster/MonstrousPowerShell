912d015e1983b0a6edfa400e94e5d17f5e5f6677
nita daniel <ndaniel@gmail.com>
2019-12-18 15:10:00 -0600

    auto copy changeset comment to clipboard; attempt autodetect of workspace; update Merge-Code help

--- TFS/mergeFunction.psm1
+++ TFS/mergeFunction.psm1
@@ -1,4 +1,4 @@
-﻿Add-Type -TypeDefinition @"
+﻿﻿Add-Type -TypeDefinition @"
     public enum BranchName
     {
         Dev,
@@ -16,38 +16,62 @@ Add-Type -TypeDefinition @"
     }
 "@
 
+$collection="http://server23:8080/tfs/Perfect10"
+$comment=''
+$tfsContent = tf workspaces /collection:'http://server23:8080/tfs/Perfect10' /format:detailed | Select-String -Pattern "$/:" -SimpleMatch | %{$_ -Replace "\$/: ",""}
+
 
 <#
 .SYNOPSIS
-Merge code in PerfectVision's projects with a slightly easier syntax than the tf command
-.DESCRIPTION
-    Streamlined command-line merges for Perfect-Vision tfs changesets
+The command will
+1) Merge code for a specified changeset or changeset range for a specific project
 
-.PARAMETER Source
-Source branch, may be one of: Dev,Test,Prod
-.PARAMETER Target
-Target branch, may be one of: Dev,Test,Prod
-.Parameter First
-The changeset number of the first changeset in the range
-.PARAMETER Last
-The changeset number of the last changeset in the range. If ommitted, it will merge only the first changeset
-.PARAMETER Force 	
-Ignores the merge history and merges the specified changes from the source into the destination, even if some or all these changes have been merged before.
-If combined with the Discard flag, will resolve any merge conflicts as keep the destination. 
-.PARAMETER Discard
-If present, tfs does not perform the merge operation, but updates the merge history to track that the merge occurred. This discards a changeset from being used for a particular merge.
-.PARAMETER LocalPath
-The local path to where you keep tfs files on your system. Defaults to "I:/tfs". Do not include trailing slashes.
-.PARAMETER tfExePath
-The full path to tf.exe on your system.
-.NOTES
+It also copies the comment associated with the first changeset in your merge to the clipboard.
+
+If you do not see your files in the Pending Changes portion of the TFS GUI, close any solutions you have open and check again.
+
+
+.DESCRIPTION
+Streamlined command-line merges for Perfect-Vision tfs changesets
+
 This commandlet makes 
      Merge-Code Dev Test 23426
  Functionally equivalent to 
      tf vc merge /recursive $/PTNWebAll/Dev $/PTNWebAll/Test  /version:C23426~C23426
 
-For more information about TFS command line info see
+.PARAMETER Source
+Source branch, may be one of: Dev,Test,Prod
+
+.PARAMETER Target
+Target branch, may be one of: Dev,Test,Prod
+
+.Parameter First
+The changeset number of the first changeset in the range
+
+.PARAMETER Last
+The changeset number of the last changeset in the range. If ommitted, it will merge only the first changeset
+
+.PARAMETER ProjectName
+One of PTNWebAll, Databases, Tracker. Default is PTNWebAll.
+
+.PARAMETER Force 	
+Ignores the merge history and merges the specified changes from the source into the destination, even if some or all these changes have been merged before.
+
+If combined with the Discard flag, will resolve any merge conflicts to retain the code in the target branch. 
+
+.PARAMETER Discard
+If present, tfs does not perform the merge operation, but updates the merge history to track that the merge occurred. This discards a changeset from being used for a particular merge.
+
+.PARAMETER LocalPath
+The local path to where you keep tfs files on your system. It attempts to find a workspace path mapped to your system. If not you'll have to specify.
+
+.PARAMETER tfExePath
+The full path to tf.exe on your system.
+
+.LINK
     https://docs.microsoft.com/en-us/azure/devops/repos/tfvc/use-team-foundation-version-control-commands?view=azure-devops
+.LINK
+    https://github.com/ohmonster/MonstrousPowerShell/tree/master/TFS
 
 #>
 
@@ -69,17 +93,15 @@ function Merge-Code{
         [Parameter(Mandatory=$false)]
         [Switch]$Discard,
         [Parameter(Mandatory=$false)]
-        [Switch]$Preview,
-        [Parameter(Mandatory=$false)]
-        [String]$LocalPath= "I:/TFS",
+        [String]$LocalPath= "$tfsContent",
         [Parameter(Mandatory=$false)]
         [String]$tfExePath = "c:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/Common7/IDE/CommonExtensions/Microsoft/TeamFoundation/Team Explorer/tf"
     )
     #"$Source $Target $First $Last $Discard"
-    $Local:sourcePath = "$localFilePath/$Project/$Source"
-    $Local:targetPath = "$localFilePath/$Project/$Target"
-    $Local:changesets = "C"+$First + "~C" + $Last
-
+    $private:sourcePath = "$LocalPath/$Project/$Source"
+    $private:targetPath = "$LocalPath/$Project/$Target"
+    $private:changesets = "C"+$First + "~C" + $Last
+	
 	
 	if ($Force -And $Discard )
 	{
@@ -98,12 +120,12 @@ function Merge-Code{
         #Write-Host "$tfExePath vc merge /recursive $Local:sourcePath $Local:targetPath /version:$changesets"
         & $tfExePath vc merge /recursive $Local:sourcePath $Local:targetPath /version:$changesets
     }
+
+    $private:info = & $tfExePath changeset $First /collection:$collection /noPrompt | Select-String -Pattern "Comment:"  -Context 1
+    $comment=$info.Context.PostContext
+    Set-Clipboard $comment
 	
     
 }
-#Source comment here:  tf changeset 31775 /collection:http://server23:8080/tfs/Perfect10 /noPrompt
-#checkin command ref https://docs.microsoft.com/en-us/azure/devops/repos/tfvc/checkin-command?view=azure-devops
 
-
-
-Export-Module Merge-Code
+Export-ModuleMember -Function Merge-Code
